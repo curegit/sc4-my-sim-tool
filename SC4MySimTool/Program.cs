73d8535d8638eec4addf7d0ad363460ea1b4944f
curegit <37978051+curegit@users.noreply.github.com>
2019-01-29 23:35:14 +0900

    Initial Commit

---
+++ SC4MySimTool/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SC4MySimTool
+{
+	public class Program
+	{
+		private static void Main(string[] args)
+		{
+		}
+	}
+}
