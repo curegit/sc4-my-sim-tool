56c75bb19291480471148334c8b7d6e358199c70
curegit <37978051+curegit@users.noreply.github.com>
2019-01-30 23:25:56 +0900

    Update Assembly Info

--- SC4MySimTool/Properties/AssemblyInfo.cs
+++ SC4MySimTool/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SC4MySimTool")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © curegit 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
